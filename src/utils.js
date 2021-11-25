c087750b3b16beed10641139b85d918ef7463b07
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-11-25 18:27:10 -0300

    cleanup: Remove config related logic

--- src/utils.js
+++ src/utils.js
@@ -12,10 +12,6 @@ const { v4: uuidv4 } = require("uuid");
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-const DEFAULT_CONFIG = {
-  directories: [],
-};
-
 const hasExtension = (file, extensions) => {
   const parts = file.split(".");
   const extension = parts[parts.length - 1].toLowerCase();
@@ -32,17 +28,6 @@ const isVideo = (file) => {
 
 const isFileSupported = (file) => {
   return isImage(file) || isVideo(file);
-};
-
-const loadConfigFile = (configFile) => {
-  if (!fs.existsSync(configFile)) {
-    fs.mkdirSync(path.dirname(configFile), { recursive: true });
-    fs.writeFileSync(configFile, JSON.stringify(DEFAULT_CONFIG));
-  }
-
-  const fileConfig = JSON.parse(fs.readFileSync(configFile));
-
-  return Object.assign(DEFAULT_CONFIG, fileConfig);
 };
 
 const maximizeImage = (image, window) => {
@@ -112,7 +97,6 @@ module.exports = {
   isImage,
   isVideo,
   isFileSupported,
-  loadConfigFile,
   maximizeImage,
   clearThumbnails,
   createFilePayload,
