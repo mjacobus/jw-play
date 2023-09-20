ef30c31d5bdeeef7bce6d7554f2b60fa5a71d5ac
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2023-09-20 08:15:52 -0300

    Add more logic to MediaFile

--- src/MediaFile.js
+++ src/MediaFile.js
@@ -1,3 +1,12 @@
+const hasExtension = (file, extensions) => {
+  const parts = file.split(".");
+  const extension = parts[parts.length - 1].toLowerCase();
+  return extensions.includes(extension);
+};
+
+const IMAGE_EXTENSIONS = ["png", "jpeg", "jpg", "gif"];
+const VIDEO_EXTENSIONS = ["mp4", "mpeg", "m4v", "mov"];
+
 class MediaFile {
   #data = {};
 
@@ -9,8 +18,24 @@ class MediaFile {
     return this.#data.id;
   }
 
+  get path() {
+    return this.#data.path;
+  }
+
   toJson() {
     return this.#data;
+  }
+
+  isImage() {
+    return hasExtension(this.path, IMAGE_EXTENSIONS);
+  }
+
+  isVideo() {
+    return hasExtension(this.path, VIDEO_EXTENSIONS);
+  }
+
+  isSupported() {
+    return this.isImage() || this.isVideo();
   }
 }
 
